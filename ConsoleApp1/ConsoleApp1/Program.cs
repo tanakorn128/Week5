93ebbca507201250420867d7d46cd0e0b5fedd2a
tanakorn128 <60030041@kmitl.ac.th>
2018-09-30 13:19:56 +0700

    update line 1, 4, 5, 2, 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,18 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+        line1:
             Console.WriteLine("Line1");
+            goto line4;
+        line2:
             Console.WriteLine("Linw2");
+            goto line9;
             Console.WriteLine("line3");
-            line4:
+        line4:
             Console.WriteLine("Line4");
+            goto line5;
+        line5:
             Console.WriteLine("Line5");
+            goto line2;
             Console.WriteLine("Line6");
-            goto line10;
             Console.WriteLine("Line7");
             Console.WriteLine("Line8");
+            line9:
             Console.WriteLine("Line9");
-            line10:
             Console.WriteLine("Line10");
 
         }
