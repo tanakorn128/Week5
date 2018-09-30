2d8529253be8d2aa6583a9aa9bb6274b9bd9465b
tanakorn128 <60030041@kmitl.ac.th>
2018-09-30 19:31:06 +0700

    ok

---
+++ throw/throw/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "sun", "mon", "tue", "wed", "thu", "fri", "is", "color", "day", "Saturday" };
+            string[] color = new string[] { "Red", "Yellow", "pink", "Green", "Orange", "Bule", "Purple" };
+            Console.Write("Input day name :");
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "sun"://sun is Sunday, color Red
+                    int i = 0;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+
+                case "mon":
+                    i = 1;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+
+                case "tue":
+                    i = 2;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+
+                case "wed":
+                    i = 3;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+
+                case "thu":
+                    i = 4;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+
+                case "fri":
+                    i = 5;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[i]);
+                    break;
+                case "sat":
+                    i = 9;
+                    Console.WriteLine("{0} {1} {2}{3}, {4} {5}", array[i], array[6], array[i], array[8], array[7], color[6]);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ throw2/throw2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class ContinueTest
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
