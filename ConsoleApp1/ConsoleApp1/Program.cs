983e6d2c563022de3c0e643b9055483130a21f78
tanakorn128 <60030041@kmitl.ac.th>
2018-09-30 13:10:44 +0700

    1.1.1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line1");
+            Console.WriteLine("Linw2");
+            Console.WriteLine("line3");
+            line4:
+            Console.WriteLine("Line4");
+            Console.WriteLine("Line5");
+            Console.WriteLine("Line6");
+            goto line10;
+            Console.WriteLine("Line7");
+            Console.WriteLine("Line8");
+            Console.WriteLine("Line9");
+            line10:
+            Console.WriteLine("Line10");
+
+        }
+    }
+}
